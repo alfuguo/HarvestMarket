1ab4ff8124ec068a4f13b86f3a70d3e07d4b1dc6
alfuguo <137233519+alfuguo@users.noreply.github.com>
2024-09-09 09:23:04 -0400

    update entity

--- src/main/java/com/harvestmarket/entity/Order.java
+++ src/main/java/com/harvestmarket/entity/Order.java
@@ -2,14 +2,10 @@ package com.harvestmarket.entity;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import jakarta.persistence.*;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import com.harvestmarket.entity.OrderStatus;
 import lombok.*;
+
 
 @Getter
 @Setter
@@ -18,6 +14,7 @@ import lombok.*;
 @Entity
 @Table(name = "orders")
 public class Order {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/harvestmarket/entity/VendorNews.java
+++ src/main/java/com/harvestmarket/entity/VendorNews.java
@@ -1,7 +1,7 @@
 package com.harvestmarket.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;   
+
 import lombok.*;
 
 @Getter
