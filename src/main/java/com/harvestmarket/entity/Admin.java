f9b01d07a3d5c71aef1fcb0300d2d96deb55b002
alfuguo <137233519+alfuguo@users.noreply.github.com>
2024-09-10 13:26:09 -0400

    fixing table

--- src/main/java/com/harvestmarket/entity/Admin.java
+++ src/main/java/com/harvestmarket/entity/Admin.java
@@ -1,19 +1,21 @@
 package com.harvestmarket.entity;
 
-import java.util.*;
-
 import jakarta.persistence.*;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Admin extends User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @OneToMany(mappedBy = "approvedBy", cascade = CascadeType.ALL)
+    private Long id;
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL)
     private List<Market> approvedMarkets = new ArrayList<>();
     @OneToMany(mappedBy = "postedBy", cascade = CascadeType.ALL)
     private List<News> postedNews = new ArrayList<>();
